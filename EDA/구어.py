185ba3eedde39a1a6468ecbcc068132b97cbcec2
Lee_JunWon <cpprhtn@naver.com>
2020-09-09 21:21:07 +0900

    9/9

--- EDA/구어.py
+++ EDA/구어.py
@@ -55,7 +55,7 @@ del df0['original_form']
  
  
  
- frames = []
+frames = []
 df_list = []
 def read_json(a):
     if a < 1:
@@ -95,3 +95,48 @@ del df0['original_form']
 
 
  df0.to_csv("spoken_language_2.csv")
+
+
+
+frames = []
+df_list = []
+def read_json(a):
+    if a < 1:
+        b = 'Error.json'
+    elif a < 10:
+        b = 'SERW190000000{}.json'.format(a)
+    elif a < 100:      
+        b = 'SERW19000000{}.json'.format(a)
+    elif a< 1000:
+        b = 'SERW1900000{}.json'.format(a)
+    elif a < 10000:
+        b = 'SERW190000{}.json'.format(a)
+    elif a < 100000:
+        b = 'SERW19000{}.json'.format(a)
+    else:
+        b = 'Error.json'
+    data = json.load(open(b))
+    data2 = data["document"]
+    result = []
+    for d in data2:
+        result.extend(d['utterance'])
+    globals()['df{}'.format(a)]  = pd.DataFrame(result)
+    
+
+for i in range(1001,75070):
+    try:
+        read_json(i)
+        df_list.append(i)
+    except FileNotFoundError:
+        print("i =", i,"\n 파일이 존재하지 않으므로 건너뜁니다.")
+        
+
+for k in df_list: 
+    frames.append(globals()['df{}'.format(k)])
+    
+df0 = pd.concat(frames)
+
+del df0['original_form']
+
+
+ df0.to_csv("spoken_language_3.csv")
